9b721911d05c1551ce405d808905b4b7f1a9f32c
filip-chodziutko <62184852+filip-chodziutko@users.noreply.github.com>
2020-12-15 17:05:36 +0100

    First Version (#6)
    * Add Base Solution
    
    * Add simple list view to display processes (#1)
    
    * Use datagrid to display process list (#2)
    
    * Add process priority combobox (#3)
    
    * Add task refreshing process list (#4)
    
    * Refresh task (#5)
    
    * Add task refreshing process list
    
    * Added change priority button

---
+++ MvxProcessManager.Core/ViewModels/ProcessManagerViewModel.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+
+namespace MvxProcessManager.Core.ViewModels
+{
+    public class ProcessManagerViewModel : MvxViewModel
+    {
+        public ProcessManagerViewModel()
+        {
+            RefreshCommand = new MvxCommand(Refresh);
+            KillCommand = new MvxCommand(Kill);
+            ChangePriorityCommand = new MvxCommand(ChangePriority);
+            KeepAliveCommand = new MvxCommand(KeepAlive);
+
+            Processes = new MvxObservableCollection<Process>(Process.GetProcesses());
+        }
+
+        public IMvxCommand RefreshCommand { get; set; }
+        public IMvxCommand KillCommand { get; set; }
+        public IMvxCommand ChangePriorityCommand { get; set; }
+        public IMvxCommand KeepAliveCommand { get; set; }
+
+        public IEnumerable<ProcessPriorityClass> ProcessPriorities => Enum.GetValues(typeof(ProcessPriorityClass)).Cast<ProcessPriorityClass>();
+
+        private double _refreshFrequency = 1.0; // seconds
+        private MvxObservableCollection<Process> _processes = new MvxObservableCollection<Process>();
+        private Process _selectedProcess;
+        private ProcessPriorityClass _selectedProcessPriority = ProcessPriorityClass.Normal;
+        private bool _doRefresh;
+        private string _processNameFilter = "";
+
+        public void Refresh()
+        {
+            IEnumerable<Process> receivedProcesses = Process.GetProcesses().Where(p => p.ProcessName.ToLower().StartsWith(_processNameFilter));
+            var tmpId = SelectedProcess?.Id;
+            Processes = new MvxObservableCollection<Process>(receivedProcesses);
+            var stillSelected = Processes.Where(p => p.Id == tmpId);
+            if (stillSelected.Any())
+            {
+                SelectedProcess = stillSelected.First();
+            }
+        }
+
+        public void Kill()
+        {
+            if (SelectedProcess != null)
+            {
+                SelectedProcess.Kill();
+                SelectedProcess = null;
+            }
+        }
+
+        public void ChangePriority()
+        {
+            if (SelectedProcess != null)
+            {
+                SelectedProcess.PriorityClass = SelectedProcessPriority;
+                RaisePropertyChanged(() => SelectedProcess);
+            }
+        }
+        
+        public void KeepAlive()
+        {
+            if (SelectedProcess != null)
+            {
+                Task keepAliveTask = new Task(() =>
+                {
+                    var process = SelectedProcess;
+                    var fileName = process.MainModule.FileName;
+                    while (true)
+                    {
+                        process.WaitForExit();
+                        process.Start();
+                    }
+                });
+                keepAliveTask.Start();
+            }
+        }
+
+        public ProcessPriorityClass SelectedProcessPriority
+        {
+            get => _selectedProcessPriority;
+            set => SetProperty(ref _selectedProcessPriority, value);
+        }
+
+        public string ProcessNameFilter
+        {
+            get => _processNameFilter;
+            set
+            {
+                SetProperty(ref _processNameFilter, value.ToLower());
+                Refresh();
+            }
+        }
+
+        public double RefreshFrequency
+        {
+            get => _refreshFrequency;
+            set => SetProperty(ref _refreshFrequency, value);
+        }
+
+        public MvxObservableCollection<Process> Processes
+        {
+            get => _processes;
+            set
+            {
+                SetProperty(ref _processes, value);
+                //RaisePropertyChanged(() => VisibleProcesses);
+            }
+        }
+
+        //public MvxObservableCollection<Process> VisibleProcesses
+        //{
+        //    get => new MvxObservableCollection<Process>(
+        //            _processes.Where(p => p.ProcessName.ToLower().StartsWith(_processNameFilter)
+        //        ));
+        //}
+
+        public Process SelectedProcess
+        {
+            get => _selectedProcess;
+            set => SetProperty(ref _selectedProcess, value);
+        }
+
+        public bool DoRefresh
+        {
+            get => _doRefresh;
+            set
+            {
+                SetProperty(ref _doRefresh, value);
+                if (value)
+                {
+                    Task refreshTask = new Task(() =>
+                    {
+                        while (_doRefresh)
+                        {
+                            Refresh();
+                            Thread.Sleep((int)RefreshFrequency*1000);
+                        }
+                        Console.Out.WriteLine("Finished");
+                    });
+                    refreshTask.Start();
+                }
+            }
+        }
+    }
+}
---
+++ MvxProcessManager.Wpf/Views/ProcessManagerView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace MvxProcessManager.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for ProcessManagerView.xaml
+    /// </summary>
+    public partial class ProcessManagerView : MvxWpfView
+    {
+        public ProcessManagerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
